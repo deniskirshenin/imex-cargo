a40f75bcace9c5344aa39a15e0aa6d410ffdccdd
deniskirshenin <deniskirshenin@gmail.com>
2023-09-10 17:16:48 +0400

    adding plugin for multiple pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,28 @@ const path = require('path')
 
 export default {
   root: path.resolve(__dirname, 'src'),
+  plugins: [
+    ViteHtmlPlugin({
+      pages: {
+        index: {
+          template: 'dist/index.html', // Specify the template HTML file
+          filename: 'index.html', // Specify the output filename
+        },
+        about: {
+          template: 'dist/about.html',
+          filename: 'about.html',
+        },
+        tracking: {
+          template: 'dist/tracking.html',
+          filename: 'tracking.html',
+        },
+        contact: {
+          template: 'dist/contact.html',
+          filename: 'contact.html',
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
