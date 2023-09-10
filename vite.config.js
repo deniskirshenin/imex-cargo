2843925539673df24d4a14f9bd9b6e3f250d37f5
deniskirshenin <deniskirshenin@gmail.com>
2023-09-10 20:08:52 +0400

    fixing vite setup to build multiple page website

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,25 @@
-const path = require('path')
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
-export default {
-  root: path.resolve(__dirname, 'src'),
-  plugins: [
-    ViteHtmlPlugin({
-      pages: {
-        index: {
-          template: 'dist/index.html', // Specify the template HTML file
-          filename: 'index.html', // Specify the output filename
-        },
-        about: {
-          template: 'dist/about.html',
-          filename: 'about.html',
-        },
-        tracking: {
-          template: 'dist/tracking.html',
-          filename: 'tracking.html',
-        },
-        contact: {
-          template: 'dist/contact.html',
-          filename: 'contact.html',
-        },
+export default defineConfig({
+  root: resolve(__dirname, 'src'),
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'src/index.html'),
+        service: resolve(__dirname, 'src/service.html'),
+        contact: resolve(__dirname, 'src/contact.html'),
+        tracking: resolve(__dirname, 'src/tracking.html'),
       },
-    }),
-  ],
+    },
+  },
   resolve: {
     alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    }
+      '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
+    },
   },
   server: {
     port: 8080,
-    hot: true
-  }
-}
+    hot: true,
+  },
+});
